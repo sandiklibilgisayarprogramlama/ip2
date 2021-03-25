172d0a9ff35a3635e0f7a71d191fd5618c91fabd
Caner BALIM <cnrblm@gmail.com>
2021-03-25 09:31:32 +0300

    arama

--- app.py
+++ app.py
@@ -3,16 +3,19 @@
 
 import datetime
 from flask import Flask,request,redirect,session
-from flask.helpers import url_for
+from flask.helpers import flash, url_for
 from flask.templating import render_template
 from database import Kullanici, Notlar
 from werkzeug.security import generate_password_hash, check_password_hash
+import locale
+locale.setlocale(locale.LC_ALL, 'tr_TR.utf8')
 
 app=Flask(__name__)
 app.secret_key="06e3af3ce8e1d5030e581fd8f038de3810ea16b7b9ea586c"
 
 @app.route("/",methods=["POST","GET"])
 def anasayfa():
+    # POST işleminde giriş yap gerçekleşiyor
     if request.method=="POST":
         kadi = request.form.get("inp_email")
         sifre = request.form.get("inp_sifre")
@@ -26,11 +29,31 @@ def anasayfa():
             kullaniciad=row.kullaniciadi
         if gelenad!="" and check_password_hash(hash_sifre,sifre):
             session["kadi"] = kullaniciad
-            return redirect("notekle")
+            return redirect("tumnotlar")
         else:
             return render_template("index.html",hata="girilen kullanıcı adı veya şifre yanlış")
         
     return render_template("index.html")
+
+@app.route("/arama",methods=["POST"])
+def arama():
+    if request.method=="POST":
+        arama_metni = request.form.get("inp_arama")
+        sonuc = Notlar.select().where(Notlar.baslik.contains(arama_metni.replace("İ","I").lower()))
+
+        return render_template("arama_sonuclari.html",aramasonuclari=sonuc)
+
+
+@app.route("/sil/<id>",methods=["GET"])
+def sil(id):
+    result= Notlar.delete_by_id(id)
+    print(result)
+    if result:
+        flash("Not başarıyla silindi !",category="error")
+    else:
+        flash("Silme esnasında bir problem var !",category="alert alert-danger")
+
+    return render_template("tumnotlar.html")
 
 @app.route("/notekle",methods=["POST","GET"])
 def notekle():
@@ -48,8 +71,17 @@ def notekle():
 
 @app.route("/tumnotlar")
 def tumnotlar():
-    notes=Notlar.select()
-    return render_template("tumnotlar.html",notlar=notes)
+    if "kadi" in session:
+        notes=Notlar.select()
+        return render_template("tumnotlar.html",notlar=notes)
+    else:
+        return redirect("/")
+
+@app.route("/cikis")
+def cikis():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect("/")
 
 @app.route("/kayitol",methods=["POST","GET"])
 def kayitol():
